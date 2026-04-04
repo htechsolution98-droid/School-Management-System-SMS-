b76727695f343b41f39ea9a6f118c62756dce90b
Mastava Yash M <167844732+YLlsh@users.noreply.github.com>
2026-04-04 18:28:53 +0530

    Change database default to use environment variable
    Updated database configuration to use DATABASE_URL from environment variables.

--- sms/settings.py
+++ sms/settings.py
@@ -92,7 +92,7 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',  # fallback for local
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
 }
